e8787ac6f3419a34e12557d8f71ed4bfd92b9594
Ablogin Egor <47031969+egor2810@users.noreply.github.com>
2023-11-18 19:42:53 +0300

    Version 0.0.1

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -7,13 +7,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    // MARK: - outlets
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redColorSlider: UISlider!
+    @IBOutlet weak var greenColorSlider: UISlider!
+    @IBOutlet weak var blueColorSlider: UISlider!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var bottomStack: UIStackView!
+    
+    // MARK: - override
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 20
+        bottomStack.layer.cornerRadius = 20
+        
+        setupSlider(redColorSlider, color: .red)
+        setupSlider(greenColorSlider, color: .green)
+        setupSlider(blueColorSlider, color: .blue)
     }
-
-
+    
+    // MARK: - actions
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        changeViewColor()
+        changeLabel(for: sender)
+    }
+    
+    // MARK: - private methods
+    private func changeViewColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setupSlider(_ slider: UISlider, color: UIColor) {
+        slider.minimumValue = 0
+        slider.value = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = color
+        
+        changeLabel(for: slider)
+    }
+    
+    private func changeLabel(for slider: UISlider) {
+        let text = String(format: "%.2f", slider.value)
+        
+        switch slider.tag {
+        case 0:
+                redValueLabel.text = text
+        case 1:
+                greenValueLabel.text = text
+        default:
+                blueValueLabel.text = text
+        }
+    }
 }
 
